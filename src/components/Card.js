c3c38091bbb4d765eb2d73f954b6a58e9150a66c
Benjamin McMahan <108589431+BenjaminMcMahan@users.noreply.github.com>
2023-05-28 16:24:55 -0400

    Implements a Card with static data

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import KatieZaferesPhoto from "../images/katie-zaferes.png"
+import Star from "../images/star.png";
+const Card = () => {
+    return (
+        <div className="card">
+            <img src={KatieZaferesPhoto} alt="Katie Zaferes" className="card--image"/>
+            <div className="card--stats">
+                <img src={Star} alt="star" className="card--star"/>
+                <span>5.0</span>
+                <span className="gray">(6) &#8226; </span>
+                <span className="gray"> USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
+    )
+};
+
+export default Card;
