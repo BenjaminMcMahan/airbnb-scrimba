893b92f4823855d8219b571304f0c989636e7ad4
Benjamin McMahan <108589431+BenjaminMcMahan@users.noreply.github.com>
2023-05-29 12:51:34 -0400

    Fixes path issue

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@
 const Hero = () => {
     return (
         <section className="hero">
-            <img src="public/images/photo-grid.png" alt="Grid of photos" className="hero--photo"/>
+            <img src="images/photo-grid.png" alt="Grid of photos" className="hero--photo"/>
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">
                 Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
