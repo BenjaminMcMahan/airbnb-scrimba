9eafb4c18fd22b3e9870f794b93b3dc874d6c950
Benjamin McMahan <108589431+BenjaminMcMahan@users.noreply.github.com>
2023-05-28 20:27:32 -0400

    Implements props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ function App() {
         <div>
             <Navbar/>
             <Hero/>
-            <Card/>
+            <Card
+                stars={5.0}
+                starsCount={6}
+                country={"USA"}
+                title={"Life Lessons with Katie Zaferes"}
+                price={136}
+            />
         </div>
     );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
 import KatieZaferesPhoto from "../images/katie-zaferes.png"
 import Star from "../images/star.png";
-const Card = () => {
+const Card = ({stars, starsCount, country, title, price}) => {
     return (
         <div className="card">
             <img src={KatieZaferesPhoto} alt="Katie Zaferes" className="card--image"/>
             <div className="card--stats">
                 <img src={Star} alt="star" className="card--star"/>
-                <span>5.0</span>
-                <span className="gray">(6) &#8226; </span>
-                <span className="gray"> USA</span>
+                <span>{stars}</span>
+                <span className="gray">({starsCount}) &#8226; </span>
+                <span className="gray"> {country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{title}</p>
+            <p><span className="bold">From ${price}</span> / person</p>
         </div>
     )
 };
